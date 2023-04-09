f5b8909293e0eec0321f0bf9204b37adc9345012
Viktor Chen <chenviktor87@gmail.com>
2023-04-09 12:40:32 +0500

    add Zadacha2

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter first number: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Enter second number: ");
+int b = int.Parse(Console.ReadLine()!);
+
+int min = a;
+int max = b;
+if (a > b)
+{
+    min = b;
+    max = a;
+}
+Console.Write("Minumum number: ");
+Console.WriteLine(min);
+Console.Write("Maximum number: ");
+Console.WriteLine(max);
