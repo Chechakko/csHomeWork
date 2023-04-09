2d4f2aee7bd44083998c36bd4e6624e1b186052b
Viktor Chen <chenviktor87@gmail.com>
2023-04-09 12:57:56 +0500

    add Zadacha4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter first number: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter second numer: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter third number: ");
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
